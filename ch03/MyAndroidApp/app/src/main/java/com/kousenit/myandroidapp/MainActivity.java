6b966fd9cb3608466fd4611a8e077e504db4231e
sby5388PC <sby5388@github.com>
2021-04-03 00:07:16 +0800

    update ch03 MyAndroidApp

--- ch03/MyAndroidApp/app/src/main/java/com/kousenit/myandroidapp/MainActivity.java
+++ ch03/MyAndroidApp/app/src/main/java/com/kousenit/myandroidapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.kousenit.myandroidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
